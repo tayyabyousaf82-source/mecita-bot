ecba3e21ef74b1649b29e9e8a2ff00563f0f3813
tayyabyousaf82-source <tayyabyousaf82@gmail.com>
2026-05-07 02:18:48 +0500

    Delete handlers/__init__.py

--- handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from handlers import common, nueva_cita, profile, admin
-
-router = Router()
-router.include_router(common.router)
-router.include_router(nueva_cita.router)
-router.include_router(profile.router)
-router.include_router(admin.router)
