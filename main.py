aa79f24313c4e9608d5d009c4bf8a3548a7dbb2f
tayyabyousaf82-source <tayyabyousaf82@gmail.com>
2026-05-09 15:08:41 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-import uvicorn
-
-logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port)
